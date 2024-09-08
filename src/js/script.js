ee575a754c846690bb9e6d2df059ca7c4aeec5ab
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-09-08 17:52:41 +0300

    add babel

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Glide('.glide', {
+        type: 'carousel',
+        focusAt: 'center',
+        startAt: 0,
+        gap: 20,
+        perView: 2,
+        autoplay: 3000,
+    }).mount();
+
+    const map = L.map('map').setView([50.4501, 30.5234], 13);
+
+    const layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const coordinates = L.marker([50.4501, 30.5234]).addTo(map)
+        .bindPopup('Киев, Украина')
+        .openPopup();
+
+})
